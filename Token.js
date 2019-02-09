46ed4ed536b6cb04483a44240dbbd050e8c2b103
Fabian Denoodt <32387007+oBoii@users.noreply.github.com>
2019-02-09 14:49:55 +0100

    Token class

---
+++ Token.js
@@ -0,0 +1,28 @@
+class Token {
+  constructor(owner, x, y) {
+    this._owner = owner
+    this._x = x
+    this._y = y
+  }
+
+  get Owner() {
+    return this._owner
+  }
+
+  get X() {
+    return this._x
+  }
+
+  set X() {
+    return this._x
+  }
+
+  get Y() {
+    return this._y
+  }
+
+  set Y() {
+    return this._y
+  }
+
+}
